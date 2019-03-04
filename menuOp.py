73b2331a02eb0175ecc740116fd92d658b9129af
Omprakash Kumawat <opja2581@gmail.com>
2019-03-04 21:32:14 +0530

    files path changed

--- menuOp.py
+++ menuOp.py
@@ -2,18 +2,18 @@
 # coding: utf-8
 	
 print("Welcome Omprakash Kumawat!!!")
-print("What is your chice.")
+print("\nWhat is your chice.")
 
 while True:
-    print("1.FaceBook\n2.Linkdin")
+    print("\n1.FaceBook\n2.Linkdin")
     
     n = input()
 
     if n == str(1):
-        import OP.OpFb
+        import Facebook #Python File Name
         break
     elif n==str(2):
-        import OP.OpLinkdin
+        import Linkdin #Python file name
         break
     else:
         print("Please Enter a right choice!!!!")
